ef1de3968c270f21515618238910d27f928dac36
Dmitry-15 <dimaggio.dima@mail.ru>
2022-03-19 19:04:15 +0300

    Задания

--- Tasks/individ1.py
+++ Tasks/individ1.py
@@ -10,7 +10,7 @@
 
 class Payment:
 
-    def __init__(self, first, second, third):
+    def __init__(self, first=0, second=0, third=0):
         self.first = first
         self.second = second
         self.third = third
@@ -21,15 +21,17 @@ class Payment:
         self.third = int(input("Введите целое число, количество дней в месяце: "))
 
     def display(self):
-        print(f"Начисленное сумма за данное количество дней: {self.first / self.third * self.second}")
+        print(f"Начисленное сумма за данное количество дней", summa(self.first, self.second, self.third))
 
-    def summa(self):
-        return self.first / self.third * self.second
+def summa(first, second, third):
+    if first == 0:
+        raise ValueError
+    else:
+        return first / third * second
 
 
 if __name__ == "__main__":
     amount = Payment(28500.5, 25, 30)
     amount.display()
     amount.read()
-    amount.summa()
     amount.display()
